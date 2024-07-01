7726a3c53035db816749925eca18851c7085b468
Will Hedges <wshedges@outlook.com>
2024-06-30 23:16:36 -0500

    moved arg parsing to utils * added new function, mouse_arg_parser()

--- lgmpm.py
+++ lgmpm.py
@@ -2,68 +2,15 @@
 # lgmpm.py - Logitech G Mouse Profile Manager
 #   a script to manage and cycle profiles for Logitech G mice with ratbagctl
 
-import argparse
 import sys
 
 from mouse import Mouse
-from utils import print_help_msg
+from utils import mouse_arg_parser, print_help_msg
 
 
 def main():
-    parser = argparse.ArgumentParser(
-        description="manages profiles for Logitech G mice using ratbagctl",
-    )
-    parser.add_argument(
-        "profile_name",
-        nargs="?",
-        type=str,
-        default="default",
-        help="the name of the profile",
-    )
-    parser.add_argument(
-        "-a",
-        "--active",
-        help="make <profile_name> the active profile",
-        action="store_true",
-    )
-    parser.add_argument(
-        "-c",
-        "--cycle",
-        help="cycle (up) to the next stored profile, if one exists",
-        action="store_true",
-    )
-    parser.add_argument(
-        "-d",
-        "--delete",
-        help="delete <profile_name>",
-        action="store_true",
-    )
-    parser.add_argument(
-        "-l",
-        "--list",
-        help="list all saved profiles for the connected mouse",
-        action="store_true",
-    )
-    parser.add_argument(
-        "-n",
-        "--new",
-        help="create a new profile with called <profile_name>",
-        action="store_true",
-    )
-    parser.add_argument(
-        "-s",
-        "--show",
-        help="show the saved settings for <profile_name>",
-        action="store_true",
-    )
-    parser.add_argument(
-        "-u",
-        "--update",
-        help="update <profile_name> with the current mouse settings",
-        action="store_true",
-    )
 
-    args = parser.parse_args()
+    args = mouse_arg_parser()
 
     # show an error if more than one flag is set
     flag_count = list(args.__dict__.values()).count(True)
@@ -78,6 +25,9 @@ def main():
     if args.active:
         mouse.set_active_profile(args.profile_name)
 
+    # TODO --cycle, --list, and --new do not need a profile_name as an arg
+    #   so should this throw an error if the user provides one?
+    #       currently works fine without any handling
     elif args.cycle:
         mouse.cycle_profile()
 
@@ -96,8 +46,8 @@ def main():
     elif args.update:
         mouse.update_profile(args.profile_name)
 
-    # if no flags are set, show a message
     else:
+        # if no flags are set, show a message
         print("No flag(s) set")
         print_help_msg()
 
